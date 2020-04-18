ee4602b95bad8c3fd1e10fd4c1b7bb67f651f65e
PatrickTelfer <patrickwktelfer@gmail.com>
2020-04-18 11:17:02 -0400

    basic treat throwing physics + player movement

---
+++ Assets/Scripts/DogMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogMove : MonoBehaviour
+{
+    public float dogMoveSpeed = 4f;
+    public bool autoWalk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (autoWalk)
+        {
+            transform.localPosition += transform.forward * dogMoveSpeed * Time.deltaTime;
+        }
+    }
+}

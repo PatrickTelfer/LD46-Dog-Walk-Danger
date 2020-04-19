f12c2a089d0ac8a3dc5be212db7b2c4cf0d52afa
PatrickTelfer <patrickwktelfer@gmail.com>
2020-04-19 18:36:54 -0400

    final polish on game, added end screen

--- Assets/Scripts/Lava.cs
+++ Assets/Scripts/Lava.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Lava : MonoBehaviour
 {
     [Range(0, 0.05f)]
     public float lavaRiseSpeed;
+    Text lavaText;
 
+    private void Start()
+    {
+        lavaText = GameObject.FindGameObjectWithTag("hud").GetComponentInChildren<Text>();
+        Debug.Log(lavaText);
+    }
     private void Update()
     {
+        Transform dog = FindObjectOfType<DogMove>().transform;
+        float distanceToDog = dog.position.y - transform.position.y;
+
+        lavaText.text = "Lava distance: " + Mathf.Floor(distanceToDog);
         if (GameController.paused == false)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y + lavaRiseSpeed, transform.position.z);
